5a95a73b7b9aafde41744990bd65823882205898
chrissie-akau <cakau9@gmail.com>
2020-06-30 11:12:39 -0700

    Form Buttons Created

---
+++ misc-app-components/src/Questions.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CustomInput, Form, FormGroup, Label } from 'reactstrap';
+
+const Questions = (props) => {
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="exampleCheckbox">Are there clearly posted signs?</Label>
+        <div>
+          <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Yes" />
+          <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="No" />
+        </div>
+      </FormGroup>
+    </Form>
+  );
+}
+
+export default Questions;
