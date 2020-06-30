b16b735376c240d3edb80512689a4d4f9d0cdccc
Austin Walker <austinsrwalker@gmail.com>
2020-06-30 14:27:28 -0700

    no

--- misc-app-components/src/Questions.js
+++ misc-app-components/src/Questions.js
@@ -12,6 +12,27 @@ const Questions = (props) => {
           <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="No" />
         </div>
       </FormGroup>
+      <FormGroup>
+        <Label for="exampleCheckbox">Do the signs have Braille</Label>
+        <div>
+          <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Yes" />
+          <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="No" />
+        </div>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleCheckbox">Is the trail 5ft wide, minimum?</Label>
+        <div>
+          <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Yes" />
+          <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="No" />
+        </div>
+      </FormGroup>
+      <FormGroup>
+        <Label for="exampleCheckbox">Is the surface hard packed? (i.e. less than one inch of loos sand)</Label>
+        <div>
+          <CustomInput type="radio" id="exampleCustomRadio" name="customRadio" label="Yes" />
+          <CustomInput type="radio" id="exampleCustomRadio2" name="customRadio" label="No" />
+        </div>
+      </FormGroup>
     </Form>
   );
 }
