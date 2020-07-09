50a43fc17ef425f67f33457a70bec07f23c6c5ba
chrissie-akau <cakau9@gmail.com>
2020-07-09 10:05:27 -0700

    Trying:

--- misc-app-components/src/Questions.js
+++ misc-app-components/src/Questions.js
@@ -4,7 +4,7 @@ class Questions extends Component {
   constructor() {
     super();
     this.state = {
-      name: "React"
+      name: ""
     };
     this.onValueChange = this.onValueChange.bind(this);
     this.formSubmit = this.formSubmit.bind(this);
@@ -25,6 +25,36 @@ class Questions extends Component {
     return (
       <>
       <form onSubmit={this.formSubmit}>
+      <h3>Is there a visible trailhead?</h3>
+        <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value="Yes"
+              checked={this.state.selectedOption === "Yes"}
+              onChange={this.onValueChange}
+            />
+            Yes
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input
+              type="radio"
+              value="No"
+              checked={this.state.selectedOption === "No"}
+              onChange={this.onValueChange}
+            />
+            No
+          </label>
+        </div>
+        <div>
+          Selected option is : {this.state.selectedOption}
+        </div>
+        <button className="btn btn-default" type="submit">
+          Submit
+        </button>
+      </form><form onSubmit={this.formSubmit}>
       <h3>Is there a visible trailhead?</h3>
         <div className="radio">
           <label>
---
+++ misc-app-components/src/TypeForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+ 
+const TypeForm = () => {
+    return <ReactTypeformEmbed url="https://cakau9.typeform.com/to/RULzAC6O" />
+}
+export default TypeForm
